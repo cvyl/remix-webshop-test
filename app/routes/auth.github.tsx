cfd26fdd4ebd1cf66f2a881c1b7fedcdf1cb25f4
跨性别 <cvyl@pm.me>
2024-05-15 11:52:23 +0200

    [-] Github oAuth removal.

--- app/routes/auth.github.tsx
+++
@@ -1,9 +0,0 @@
-import type { ActionFunction } from "@remix-run/node";
-import { auth } from "~/utils/providers/auth.github.server";
-
-export const action: ActionFunction = async ({ request }) => {
-  return await auth.authenticate("github", request, {
-    successRedirect: "/private",
-    failureRedirect: "/",
-  });
-};
